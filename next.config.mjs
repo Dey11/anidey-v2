a84bdbc486df024277ae683f85fb7ed9c5ee44b9
Dey11 <shreyandey11@gmail.com>
2024-06-17 09:04:16 +0530

    whitelist anilist images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s4.anilist.co",
+      },
+    ],
+  },
+};
 
+// https://s4.anilist.co/file/anilistcdn/media
 export default nextConfig;
