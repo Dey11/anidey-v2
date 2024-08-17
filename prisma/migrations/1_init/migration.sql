344a48cc75981ba87370d9457bfdedeaa72b275f
Dey11 <shreyandey11@gmail.com>
2024-08-18 04:23:57 +0530

    fixed prisma migrations

--- prisma/migrations/1_init/migration.sql
+++ prisma/migrations/1_init/migration.sql
@@ -50,11 +50,57 @@ CREATE TABLE "WatchingList" (
     "anilistId" INTEGER NOT NULL,
     "image" TEXT DEFAULT '',
     "timestamp" INTEGER NOT NULL DEFAULT 0,
+    "duration" INTEGER NOT NULL DEFAULT 0,
     "userId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "WatchingList_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "History" (
+    "id" TEXT NOT NULL,
+    "animeId" TEXT NOT NULL,
+    "episodeId" TEXT NOT NULL,
+    "episodeNo" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "isFiller" BOOLEAN NOT NULL DEFAULT false,
+    "anilistId" INTEGER NOT NULL,
+    "image" TEXT DEFAULT '',
+    "isComplete" BOOLEAN NOT NULL DEFAULT false,
+    "timestamp" INTEGER NOT NULL DEFAULT 0,
+    "duration" INTEGER NOT NULL DEFAULT 0,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "History_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Favourite" (
+    "id" TEXT NOT NULL,
+    "anilistId" INTEGER NOT NULL,
+    "animeTitle" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "totalEpisodes" INTEGER NOT NULL DEFAULT 0,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Favourite_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Errors" (
+    "errorId" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "url" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Errors_pkey" PRIMARY KEY ("errorId")
 );
 
 -- CreateIndex
@@ -66,6 +112,12 @@ CREATE UNIQUE INDEX "Session_sessionToken_key" ON "Session"("sessionToken");
 -- CreateIndex
 CREATE UNIQUE INDEX "WatchingList_episodeId_key" ON "WatchingList"("episodeId");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "History_episodeId_key" ON "History"("episodeId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Favourite_anilistId_key" ON "Favourite"("anilistId");
+
 -- AddForeignKey
 ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
@@ -74,4 +126,10 @@ ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId"
 
 -- AddForeignKey
 ALTER TABLE "WatchingList" ADD CONSTRAINT "WatchingList_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "History" ADD CONSTRAINT "History_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Favourite" ADD CONSTRAINT "Favourite_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
--- prisma/migrations/20240817031031_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `duration` on the `WatchingList` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "WatchingList" DROP COLUMN "duration";
